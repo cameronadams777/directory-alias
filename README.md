37f4e234e6cc0af7fb19d68d449a010603f46932
Cameron Adams <cameron.adams@hey.com>
2022-08-03 15:06:02 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Dira
+
+A CLI tool for aliasing directories for faster directory switching
+
+## Config
+
+The config file for Dira is located at `$HOME/.config/dira/config.json`
+
+## Usage
+
+```sh
+
+pwd # OUTPUT: /Users/cameron/dev/my-cool-project
+
+dira --alias mcp
+
+dira mcp # OUTPUT: cd "/Users/cameron/dev/my-cool-project"
+
+dira my-cool-project # OUTPUT: Alias does not exist.
+
+...
+
+```
---
+++ src/main.rs
@@ -0,0 +1,62 @@
+use clap::Parser;
+use serde_json::Value;
+use std::{fs, path::Path};
+
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+struct Cli {
+  path_alias: Option<String>,
+  #[clap(short, long, value_parser, default_value = "")]
+  alias: String,
+}
+
+const CONFIG_FILE_NAME: &str = "config.json";
+
+fn main() {
+  let args = Cli::parse();
+
+  let home_path = home::home_dir()
+    .unwrap()
+    .into_os_string()
+    .into_string()
+    .unwrap();
+
+  let config_path = format!("{}{}", home_path, "/.config/dira/");
+
+  let config_file_path = format!("{}{}", config_path, CONFIG_FILE_NAME);
+
+  if !Path::new(&config_file_path).is_file() {
+    fs::create_dir(config_path).unwrap();
+    fs::write(&config_file_path, "{}").unwrap();
+  }
+
+  let data = fs::read_to_string(&config_file_path).unwrap();
+
+  let mut config: Value = serde_json::from_str(data.as_str()).unwrap();
+
+  let alias = &args.alias;
+
+  if alias.chars().count() > 0 {
+    let cwd = std::env::current_dir()
+      .unwrap()
+      .into_os_string()
+      .into_string()
+      .unwrap();
+    config[alias] = serde_json::Value::String(cwd);
+    let config_as_string = serde_json::to_string_pretty(&config).unwrap();
+    fs::write(&config_file_path, config_as_string).unwrap();
+    return;
+  }
+
+  let path_alias = &args.path_alias.unwrap();
+
+  println!("{}", path_alias);
+
+  let path_by_alias = config.get(path_alias).expect("Alias does not exist.");
+
+  println!("{}", path_by_alias.to_string());
+
+  let command_str = format!("cd {}", path_by_alias);
+
+  println!("{}", command_str);
+}
